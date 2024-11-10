f996c9c2cbb1eaea5c9b5bd215d18995f4b56f6a
TimNevzor <timofeynevzorov868@gmail.com>
2024-11-10 17:53:52 +0300

    fixed echo and server

--- echo1.cpp
+++ echo1.cpp
@@ -1,37 +1,53 @@
-//daytime.cpp
+//echo1.cpp
 #include <iostream>
-#include <arpa/inet.h> //inet_addr() htons()
-#include <unistd.h> //socket(), sendto() recvfrom()
+#include <cstring> // strlen
+#include <arpa/inet.h> // inet_addr htons
+#include <unistd.h> // socket bind connect send recv close
 using namespace std;
 int main()
 {
-    char msg[] = "Good morning, mr sir\n"; //сообщение, к-ое отправится пользователю
-    struct sockaddr_in addr; //настройка структуры
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(3306);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    int s = socket(AF_INET, SOCK_DGRAM, 0); //создание сокета UDP
+    char buf[256];
+    char msg[] = "Какой чудесный день, чтобы делать практику!\n";
+    struct sockaddr_in self_addr;
+    self_addr.sin_family = AF_INET;
+    self_addr.sin_port = 0; // Автоматический выбор порта
+    self_addr.sin_addr.s_addr = INADDR_ANY; // Принять соединения от любого адреса
+    struct sockaddr_in srv_addr;
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(1234); // Порт сервера
+    srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int s = socket(AF_INET, SOCK_STREAM, 0); //создает сокет для TCP-соединения
     if (s == -1) {
         cerr << "Error creating socket" << endl;
         return 1;
-    }//отправляет сообщение
-    ssize_t sent_bytes = sendto(s, msg, sizeof(msg), 0, (const struct sockaddr*)&addr, sizeof(addr));
-    if (sent_bytes == -1) {
-        cerr << "Error sending data" << endl;
-        close(s); 
+    }
+    int e = bind(s, (struct sockaddr*)&self_addr, sizeof(self_addr)); //привязывает сокет к локальному адресу и порту
+    if (e == -1) {
+        cerr << "Error binding socket" << endl;
+        close(s);
         return 1;
     }
-    cout << "We send: " << msg; //выводит сообщение
-    char buf[256]; //получает ответ от сервера
-    socklen_t addr_len = sizeof(addr);
-    ssize_t recv_len = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*)&addr, &addr_len);
-    if (recv_len == -1) {
+    e = connect(s, (struct sockaddr*)&srv_addr, sizeof(srv_addr)); //подключает сокет к серверу по указанному адресу и порту
+    if (e == -1) {
+        cerr << "Error connecting socket" << endl;
+        close(s);
+        return 1;
+    }
+    e = send(s, msg, strlen(msg), 0); //отправляет сообщение серверу
+    if (e == -1) {
+        cerr << "Error sending socket" << endl;
+        close(s);
+        return 1;
+    }
+    cout << "Отправлено: " << msg; //выводит отправленное сообщение в консоль
+    int rc = recv(s, buf, sizeof(buf), 0); // получает ответ от сервера и сохраняет его в буфере buf
+    if (rc < 0) {
         cerr << "Error receiving data" << endl;
         close(s);
         return 1;
     }
-    buf[recv_len] = '\0';
-    cout << "We receive: " << buf; //выводит ответ
-    close(s); //закрывает сокет
+    buf[rc] = '\0'; // добавляет завершающий нулевой символ в конец буфера
+    cout << "Получено: " << buf; //выводит полученный ответ в консоль
+    close(s); // закрывает сокет после завершения передачи данных
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,57 +1,31 @@
 //server.cpp
 #include <iostream>
-#include <ctime>
-#include <arpa/inet.h>
-#include <unistd.h>
-using namespace std;
-
+#include <unistd.h> // socket bind connect send recv close
+#include <arpa/inet.h> // htons
 int main()
 {
-    int s = socket(AF_INET, SOCK_DGRAM, 0); // создание сокета UDP
-    if (s == -1) {
-        cerr << "Error creating socket" << endl;
-        return 1;
-    }
+    int PORT = 1234;
+    int buf_size = 256;
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    struct sockaddr_in server_addr; // настройка структуры
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(3306);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int addrlen = sizeof(address);
+    char buf[buf_size];
 
-    if (bind(s, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1) {
-        cerr << "Error binding socket" << endl;
-        close(s);
-        return 1;
-    }
+    int s = socket(AF_INET, SOCK_STREAM, 0); // создает сокет для TCP-соединения
+
+    bind(s, (struct sockaddr *)&address, sizeof(address)); // привязывает сокет к локальному адресу и порту
+    listen(s, 3); // переводит сокет в режим прослушивания и указывает максимальное количество ожидающих соединений
 
     while (true) {
-        struct sockaddr_in client_addr;
-        unsigned int addr_len = sizeof(client_addr);
-        char buf[256];
-
-        // получает сообщение от клиента
-        ssize_t rc = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr*)&client_addr, &addr_len);
-        if (rc == -1) {
-            cerr << "Error receiving data" << endl;
-            close(s);
-            return 1;
-        }
-
-        buf[rc] = '\0'; //добавляет завершающий нулевой символ в конец buf
-        cout << buf; // выводит полученное сообщение
-
-        time_t current_time = time(0); // получает текущее время
-        string datetime = ctime(&current_time); // преобразует время в строку
-
-        // отправляет время
-        ssize_t sent_bytes = sendto(s, datetime.c_str(), datetime.length(), 0, (struct sockaddr*)&client_addr, addr_len);
-        if (sent_bytes == -1) {
-            cerr << "Error sending data" << endl;
-            close(s);
-            return 1;
-        }
+        // принимает входящее соединение от клиента и создает новый сокет new_socket для связи
+        int new_socket = accept(s, (struct sockaddr *)&address, (socklen_t*)&addrlen); 
+        recv(new_socket, buf, buf_size, 0); // получает данные от клиента и сохраняет их в буфере buf
+        send(new_socket, buf, buf_size, 0); // отправляет данные обратно клиенту
+        std::cout << buf; // выводит полученные данные
+        close(new_socket); // закрывает сокет
     }
-
-    close(s); // закрывает сокет
     return 0;
 }
